9d64bd51503e376d3c062d168e69f0f5445d6b37
Adrian Kühnis <info@azular.com>
2016-10-21 07:21:27 +0200

    fix be routing

--- tinymce4/config/routing.php
+++ tinymce4/config/routing.php
@@ -1,6 +1,6 @@
 <?php
 
-if (rex::isBackend()) {
+if (rex::isBackend() && null !== rex::getUser()) {
     $container->addRoute('/image/index', '\Tinymce4\Controller\ImageController:indexAction');
     $container->addRoute('/image/list', '\Tinymce4\Controller\ImageController:listAction');
     $container->addRoute('/file/index', '\Tinymce4\Controller\FileController:indexAction');
@@ -8,7 +8,6 @@ if (rex::isBackend()) {
     $container->addRoute('/media/index', '\Tinymce4\Controller\MediaController:indexAction');
     $container->addRoute('/media/list', '\Tinymce4\Controller\MediaController:listAction');
     $container->addRoute('/asset_js', '\Tinymce4\Controller\AssetController:jsAction');
-    //$container->addRoute('/asset_css', '\Tinymce4\Controller\AssetController:cssAction');
     $container->addRoute('/profile/index', '\Tinymce4\Controller\ProfileController:indexAction');
     $container->addRoute('/profile/edit', '\Tinymce4\Controller\ProfileController:editAction');
     $container->addRoute('/profile/remove', '\Tinymce4\Controller\ProfileController:removeAction');
