b93671855a7b6b69befff6df9d2e861a0d53f218
Alex Platter <alex.platter@gmail.com>
2018-08-07 18:16:42 +0200

    assets fixed

--- views/frontend/top.php
+++ views/frontend/top.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <script type="text/javascript" src="<?php echo rex_url::addonAssets('tinymce4', 'jquery/jquery.min.js'); ?>"></script>
+    <script type="text/javascript" src="<?php echo rex_url::coreAssets('jquery.min.js'); ?>"></script>
     <script type="text/javascript" src="<?= rex_url::addonAssets('tinymce4', 'vue/vue.min.js'); ?>"></script>
     <link rel="stylesheet" href="<?= rex_url::addonAssets('be_style', 'css/styles.css'); ?>" type="text/css" media="screen,print"/>
     <link rel="stylesheet" href="<?= rex_url::addonAssets('tinymce4', 'backend.css'); ?>?ts=<?= filemtime(rex_path::addonAssets('tinymce4', 'backend.css')) ?>" type="text/css" media="screen,print"/>
