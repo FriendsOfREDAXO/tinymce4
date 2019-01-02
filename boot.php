9a164c564930ea158affc245670efa5e7bd6d937
Markus Staab <maggus.staab@googlemail.com>
2019-01-02 13:44:55 +0100

    Kommentierten code entfernt

--- boot.php
+++ boot.php
@@ -1,18 +1,4 @@
 <?php
-/*
-spl_autoload_register(function ($class) {
-    $prefix = 'Tinymce4';
-    $base_dir = dirname(__FILE__).'/src';
-
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) return;
-    $relative_class = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-    if (file_exists($file)) {
-        require $file;
-    }
-});
- */
 
 if (rex::isBackend() && isset($_REQUEST['page']) && !isset($_REQUEST['_pjax'])) {
     // Tinymce core
